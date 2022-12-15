64aecfd9dbc0c6b69d65df7fea818db90eb9c689
UTP <utp@iMac-de-Administrador-6.local>
2022-12-15 16:09:10 -0500

    Background

--- main.js
+++ main.js
@@ -3,18 +3,34 @@ let movesArray = new Array(9).fill(null);
 const juega = document.querySelector('.turnDisplayer');
 let x=0, o=0;
 
+
+
 function newGame(){
-    turn = true
-    document.querySelector('.table').innerHTML = '<div class="box"></div><div class="box"></div><div class="box"></div><div class="box"></div><div class="box"></div><div class="box"></div><div class="box"></div><div class="box"></div><div class="box"></div>'
+    turn = true;
+    isOver = false;
+    const table = document.querySelector('.table');
+    table.innerHTML = '<div id="1" class="box"></div><div id="2" class="box"></div><div id="3" class="box"></div><div id="4" class="box"></div><div id="5" class="box"></div><div id="6" class="box"></div><div id="7" class="box"></div><div id="8" class="box"></div><div id="9" class="box"></div>'
     const boxlist = document.querySelectorAll('.table .box');
+
     if (!turn){
         juega.innerText = 'Juega O';
     }else{
         juega.innerText = 'Juega X';
     }
-    boxlist.forEach( (box, index) => {
-        turn = true;
-        box.addEventListener('click', () =>{
+
+    table.addEventListener('click', (event)=>{
+        if (isOver){
+            return;
+        }
+        const box = event.target;
+        const index = box.id;
+        console.log(index);
+
+        if (index>9 && index<1){
+            return;
+        }
+
+        if (movesArray[index-1] == null){
             if (turn){
                 box.classList.add('mark-x');
                 juega.innerText = 'Juega O'
@@ -22,24 +38,28 @@ function newGame(){
                 box.classList.add('mark-o')
                 juega.innerText = 'Juega X'
             }
-            movesArray[index] = turn;
+            movesArray[index-1] = turn;
+            turn = !turn;
+        }else{
+            void(0);
+        }
+        console.log(movesArray);
+        console.log(turn);
 
-            const mayWinner = numberWin();
-            if (mayWinner){
-                const lineWinner = document.createElement('div');
-                lineWinner.classList.add('line');
-                lineWinner.classList.add('line-winner-'+mayWinner);
-                document.querySelector('.table').append(lineWinner);
-                if (turn) x++
-                else o++;
-            }
+        const mayWinner = numberWin();
+        if (mayWinner){
+            const lineWinner = document.createElement('div');
+            lineWinner.classList.add('line');
+            lineWinner.classList.add('line-winner-'+mayWinner);
+            document.querySelector('.table').append(lineWinner);
+            if (!turn) x++
+            else o++;
+            isOver = true;
+        }
 
-            document.querySelector('#x').innerText = x
-            document.querySelector('#o').innerText = o
-            
+        document.querySelector('#x').innerText = x
+        document.querySelector('#o').innerText = o
 
-            turn = !turn
-        }, {once: true})
     });
 }
 function winner(i, j, k){
